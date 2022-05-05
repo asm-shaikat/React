c1d5e29fb0cfb846639990111124c749489964fd
Abu Shadat Shaikat <asmshaikat1952@gmail.com>
2022-05-05 21:18:10 +0600

    JSX

---
+++ jsx/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import {hlw, hlw1} from './main';
+// import head from './Head';
+
+
+function App(){
+  return(
+    <>
+    {hlw()}
+    {hlw1()}
+    </>
+  )
+}
+
+export default App;
